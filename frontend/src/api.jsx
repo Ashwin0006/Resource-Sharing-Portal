22d2f366bafeaf5f61bcc7101294d36efb246e21
Ashwin0006 <ashcrafts0006@gmail.com>
2025-11-23 17:33:40 +0530

    Fixed baseURL bug

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  // baseURL: "https://resource-sharing-portal.onrender.com/api",
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://resource-sharing-portal.onrender.com/api",
+  // baseURL: "http://localhost:5000/api",
 });
